afdf83515023ec67b68326d28658e51bc9afd90a
eva-kaloki <evatindi@gmail.com>
2022-12-14 12:57:51 -0800

    Question 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
 
 /**
  * print_to_98 - print all natural numbers from n to 98
- * n: the number to start counting from n to 98
+ * @n: the number to start counting from n to 98
  * Return: Always 0
  */
 
