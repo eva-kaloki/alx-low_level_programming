bb85927e4742ea327ec79182752f6d73ef0098fb
eva-kaloki <evatindi@gmail.com>
2022-12-14 08:55:41 -0800

    Question 3

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- *main - Entry point 
  *_isalpha - Return 1 if c is a letter. lowercase or uppercase
- * c: The int to print
- * Return: Always 0
+ * @c:unsigned int value to be compared with ASCII value
+ * Return: 1 if its alphabet otherwise 0
  */
 
 int _isalpha(int c)
