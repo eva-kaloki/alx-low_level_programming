cc548351018599e4b6a65a4e843b3c9b50f976c5
eva-kaloki <evatindi@gmail.com>
2022-12-14 09:45:06 -0800

    Question six

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
- * _abs - value absolute
- * n: The int to print
- * Return: Always 0
+ * _abs - computes the absolute value of an integer
+ * @n: The int to be operated upon
+ * Return: returns an unsigned int value to calling function
  */
 
 int _abs(int n)
 {
-	if (n <0)
+	if (n < 0)
 	{
 		return (n * (-1));
 	}
@@ -23,7 +23,5 @@ int _abs(int n)
 	{
 		return (n);
 	}
-	
-	
 }
 
